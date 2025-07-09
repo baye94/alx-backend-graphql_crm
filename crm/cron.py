6d4e50b8db896b2107bba7e6a8bd0f4469ddfeea
Baye Cheikh FALL <fallcheikh@finetechgroupe.com>
2025-07-09 19:17:57 +0000

    sd

--- crm/cron.py
+++ crm/cron.py
@@ -1,10 +1,9 @@
-# crm/cron.py
 import requests
 import json
 from datetime import datetime
 
 def update_low_stock():
-    endpoint = "http://localhost:8000/graphql/"  # Assure-toi que c'est l'URL correcte
+    endpoint = "http://localhost:8000/graphql/"
 
     mutation = """
     mutation {
@@ -25,19 +24,18 @@ def update_low_stock():
 
     response = requests.post(endpoint, headers=headers, json={"query": mutation})
 
-    log_file = "/tmp/low_stock_updates_log.txt"
-    with open(log_file, "a") as log:
-        log.write(f"\n[{datetime.now()}] Cron Job Execution:\n")
+    log_path = "/tmp/lowstockupdates_log.txt"
+    with open(log_path, "a") as log:
+        log.write(f"\n[{datetime.now()}] Low stock update job:\n")
         if response.status_code == 200:
             data = response.json()
             if "errors" in data:
-                log.write("Errors:\n")
-                log.write(json.dumps(data["errors"], indent=2))
+                log.write("GraphQL errors:\n")
+                log.write(json.dumps(data["errors"], indent=2) + "\n")
             else:
-                products = data["data"]["updateLowStockProducts"]["updatedProducts"]
-                message = data["data"]["updateLowStockProducts"]["message"]
-                log.write(message + "\n")
-                for p in products:
-                    log.write(f" - {p['name']}: stock = {p['stock']}\n")
+                result = data["data"]["updateLowStockProducts"]
+                log.write(result["message"] + "\n")
+                for p in result["updatedProducts"]:
+                    log.write(f"- {p['name']} => stock: {p['stock']}\n")
         else:
-            log.write(f"Request failed: HTTP {response.status_code}\n")
+            log.write(f"Request failed with status code: {response.status_code}\n")
